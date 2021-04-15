c121cfc6bffcf9c0443b485a45b600e11e32cb23
Administrator <Administrator@VM-ROMMEL>
2021-04-15 04:44:37 +0300

    create body report in html and attachment

--- send_email.ps1
+++ send_email.ps1
@@ -1,16 +1,35 @@
-﻿$Username = "employee@company.com";
-$Password = "123456789";
-#$path = "C:\txtfile.txt";
+﻿$Username = "abc@company.com.sa";
+$Password = "abc123";
 $Logfile_name = (get-date).AddDays(-1).ToString("yyyy_MM_dd")+"_sql_backup"
-$Logfile = "c:\$Logfile_name.txt"
+$Logfile = "c:\sql_logs\$Logfile_name.txt"
 $path = $Logfile
+$searchinfolder ='\\192.168.0.242\sql-backups\'
+
+#function to send report via email.
 function Send-ToEmail([string]$email, [string]$attachmentpath){
 
     $message = new-object Net.Mail.MailMessage;
-    $message.From = "employee@company.com";
+    $message.From = "abc@company.com.sa";
     $message.To.Add($email);
+    
     $message.Subject = $Logfile_name;
-    $message.Body = "Hey got the attached report of your SQL backups.";
+    $message.Body = "Hey, These are the SQL backups on 192.168.0.242 for yesterday ("+$Logfile_name+ ")`r`n";
+    $parentdirectory = Get-ChildItem -Path $searchinfolder #-Filter '*2020_12_21*'
+     $counter = 0
+    foreach ($directory in $parentdirectory) 
+    {
+    $searchbackupfileinfolder = Get-ChildItem -Path $directory.FullName    
+    $dte = (get-date).AddDays(-1).ToString("yyyy_MM_dd")
+    $today =$dte #-Format "yyyy_MM_dd"
+  
+     if( $searchbackupfileinfolder -like "*"+$today.toString()+"*")
+     {
+      $counter+=1
+     $message.Body +="`r`n"+$counter+". "+$directory.Name+".bak" 
+     echo $directory.Name
+     }
+    }
+    $message.Body += "`r`n`r`nThis is system generated email built on powershell script.`r`n This auto-notification runs every x.`r`n To unsubscribe send me a feedback.";
     $attachment = New-Object Net.Mail.Attachment($attachmentpath);
     $message.Attachments.Add($attachment);
 
@@ -18,8 +37,8 @@ function Send-ToEmail([string]$email, [string]$attachmentpath){
     $smtp.EnableSSL = $true;
     $smtp.Credentials = New-Object System.Net.NetworkCredential($Username, $Password);
     $smtp.send($message);
-    write-host "Mail Sent" ; 
+    write-host "Mail Sent |V|" ; 
     #make logger here
     $attachment.Dispose();
  }
-Send-ToEmail  -email "myemail@gmail.com" -attachmentpath $path;
+Send-ToEmail  -email "recipient@gmail.com" -attachmentpath $path;
